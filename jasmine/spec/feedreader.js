80afa4d3542ef970f3a96d05855b14d6c8fb5a4e
Erik Peterson <erik@erik-id.com>
2018-12-13 16:08:11 -0500

    test name is defined in allFeeds array

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('name defined', function() {
+           allFeeds.forEach(function(feed) {
+              expect(feed.name).toBeDefined();
+              expect(feed.url).not.toBe('');
+            });
+         });
     });
 
 
